bd7bc341f443ff77ca8c846e726f5aced2dfa752
Bhavyanth Kolli <bhavyanthkolli@Bhavyanths-MacBook-Pro.local>
2017-10-27 17:30:37 +0530

    Implented Data Tree

--- platforms/browser/www/screens/index.js
+++ platforms/browser/www/screens/index.js
@@ -31,6 +31,35 @@ var app = {
         alert('xxx');
         alert('1');
         alert(device.uuid);
+        var callbackFn = function(location) {
+            console.log('[js] BackgroundGeolocation callback:  ' + location.latitude + ',' + location.longitude);
+    
+            // Do your HTTP request here to POST location to your server.
+            // jQuery.post(url, JSON.stringify(location));
+    
+            /*
+            IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+            and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+            IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+            */
+            backgroundGeolocation.finish();
+        };
+    
+        var failureFn = function(error) {
+            console.log('BackgroundGeolocation error');
+        };
+    
+        // BackgroundGeolocation is highly configurable. See platform specific configuration options
+        backgroundGeolocation.configure(callbackFn, failureFn, {
+            desiredAccuracy: 10,
+            stationaryRadius: 10,
+            distanceFilter: 30,
+            interval: 60000,
+            activityType: "AutomotiveNavigation"
+        });
+    
+        // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+        backgroundGeolocation.start();
     },
 
     // Update DOM on a Received Event
